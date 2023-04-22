602b23b6a9ab7a87104315fef5e306074905d991
Jatin Rokde <jar.jatin.rokde2000@gmail.com>
2023-04-22 18:40:12 +0530

    Print any one Subsequence with sum K

---
+++ 13 Backtracking/03_Subsequence_with_sum_k_2.cpp
@@ -0,0 +1,65 @@
+// Printing any 1 subsequence whose sum = k;
+
+// Structure of the Code
+
+// func()
+//     if (condition statisfies)
+//         return true;
+//     else 
+//         return false;
+
+//     if (func() == true);
+//         return true;    
+
+//     return false
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool subsequenceK(int index, vector<int>& ans, int csum, int sum, int *arr, int n)
+{
+    if (index == n)
+    {
+        // if condition statisfies - return true
+        if (csum == sum)
+        {
+            for (auto it : ans)
+            {
+                cout << it << " ";
+            }
+            cout << endl;
+            return true;
+        }
+        // else return false
+        return false;
+    }
+
+    ans.push_back(arr[index]);
+    csum += arr[index];
+    if (subsequenceK(index + 1, ans, csum, sum, arr, n) == true)
+    {
+        return true;
+    }
+
+    csum -= arr[index];
+    ans.pop_back();
+    if(subsequenceK(index + 1, ans, csum, sum, arr, n) == true)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+int main(void)
+{
+    int arr[] = {1, 2, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int sum = 2;
+    vector<int> ans;
+
+    subsequenceK(0, ans, 0, sum, arr, n);   
+    return 0;
+}
