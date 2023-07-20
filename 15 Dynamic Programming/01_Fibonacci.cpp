601c5dbc5f3170bfb0438d3d35b310200b8bb339
Jatin Rokde <jar.jatin.rokde2000@gmail.com>
2023-07-20 19:17:23 +0530

    Fibonacci - Tabulation Approach

--- 15 Dynamic Programming/01_Fibonacci.cpp
+++ 15 Dynamic Programming/01_Fibonacci.cpp
@@ -16,11 +16,28 @@ int fibonacci(int n, vector<int> &dp)
 // Time Complexity = O(n)
 // Space Complexity = O(n) + O(n)
 
+// DP - Tabulation - Bottom up approach
+int fibonacci(int n)
+{
+    vector<int> dp(n + 1, -1);
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+}
+// Time Complexity = O(n)
+// Space Complexity = O(n) (No extra recursion stack space is used)
+
 int main(void)
 {
     int n;
     cin >> n;
     vector<int> dp(n + 1, -1);
-    cout << fibonacci(n, dp);
+    // cout << fibonacci(n, dp);
+    cout << fibonacci(n);
     return 0;
 }
