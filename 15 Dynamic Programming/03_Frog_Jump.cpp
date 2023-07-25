82d86559d48d4e1090675fa195cba5c434d439f2
Jatin Rokde <jar.jatin.rokde2000@gmail.com>
2023-07-26 01:31:11 +0530

    Frog Jump - Tabulation Approach

--- 15 Dynamic Programming/03_Frog_Jump.cpp
+++ 15 Dynamic Programming/03_Frog_Jump.cpp
@@ -22,12 +22,36 @@ int frogJump(int n, vector<int> &heights, vector<int> &dp)
 // Time Complexity = O(n)
 // Space Complexity = O(n) + O(n)
 
+int frogJump(int n, vector<int> &heights)
+{
+    vector<int> dp(n, -1);
+    dp[0] = 0; // define the base case
+
+    // begin the loop in the opposite order of recursion
+    for (int i = 1; i < n; i++)
+    {
+        int oneJump = dp[i - 1] + abs(heights[i] - heights[i - 1]);
+
+        int twoJump = INT_MAX;
+        if (i > 1)
+        {
+            twoJump = dp[i - 2] + abs(heights[i] - heights[i - 2]);
+        }
+        dp[i] = min(oneJump, twoJump);
+    }
+    // the last index will store the final ans
+    return dp[n - 1];
+}
+// Time Complexity = O(n)
+// Space Complexity = O(n)
+
 int main(void)
 {
     vector<int> heights{10, 20, 30, 10};
     int n = heights.size();
-    vector<int> dp(n, -1);
-    cout << frogJump(n-1, heights, dp);
+    // vector<int> dp(n, -1);
+    // cout << frogJump(n - 1, heights, dp);
+    cout << frogJump(n, heights);
 
     return 0;
 }
